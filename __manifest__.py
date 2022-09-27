9213cc29e98c9c77056e76dbd1684fe9662934ea
Marvin Kaaya <marvkaay@gmail.com>
2022-09-27 14:09:25 +0300

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name":"Library App",
+    "summary":"Manage library catalog and book lending.",
+    "author": "Kaaya Marvin",
+    "license": "AGPL-3",
+    "category":"Services/library",
+    "website": "https://github.com/kmarv",
+    "version": "15.0.1.0.0",
+    "depends": ["base"],
+    "application": True,
+    "data":[
+        "security/library_security.xml",
+        "views/library_menu.xml",
+        ],
+}
